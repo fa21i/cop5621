9cd8e79e3f99dac4517a663c055b4951bfbcd971
Andrew John Keck <keck@linprog7.cs.fsu.edu>
2022-10-04 12:50:15 -0400

    ast code

--- comp.c
+++ comp.c
@@ -1,8 +1,22 @@
 #include "y.tab.h"
 #include "ast.h"
 int yyparse();
+
+int demo(struct ast* node)
+{
+        //inclass demo function
+        printf("visit: %s\n", node->token);
+        return 0;
+}
+
 int main (int argc, char **argv) {
         int retval = yyparse();
+        //TODO: semantic analysis: symbol table + well-form, type check, etc
+        //AST -> CFG
+        //optims
+        //code generation
+
+
         if (retval == 0) print_ast();
         free_ast();
         return retval;
