e7430d2e4c4f7fcbceb676e38f47100b53355c99
Faraz Ahmad <faraz.lebro@gmail.com>
2022-10-10 14:38:46 -0400

    added declaration, initialize and scope errors

--- comp.c
+++ comp.c
@@ -1,102 +1,171 @@
 #include "y.tab.h"
 #include "ast.h"
 int yyparse();
+char* args[20];
+int types[20];
+int scope[20][2];
+char* fun_scope[20];
+int arg_c = 0;
+int type_c = 0;
+
+int print_array(int a[]){
+   for (int i = 0; i < type_c; i++)
+   {
+      printf("index: %d, ",a[i]);
+   }  
+   printf("\n");
+   return 0;
+}
+int print_char_array(char* a[]){
+   
+   for (int i = 0; i < arg_c; i++)
+   {
+      printf("index: %s, ",a[i]);
+   }  
+   printf("\n");
+   return 0;
+}
+int print_scope(int a[][2], int index){
+   for (int i = 0; i < arg_c; i++)
+   {
+      printf("scope %d: %d, ",index,a[i][index]);
+   }  
+   printf("\n");
+   return 0;
+}
+int return_index(char* a[], char* str){
+   for(int i=0;i<arg_c;i++){
+      if(strcmp(a[i],str)==0){
+         return i;
+      }
+   }
+   return -1;
+}
 
 int demo(struct ast* node)
 {
-        
-       /*Semantic checking
-           each variable v can be used only in two cases
+   if(node->ntoken == DEFINE){
+      printf("visit fun: %s\n", node->token);
+      int how_many_children = get_child_num(node);
+      printf("how_many_children: %d\n", how_many_children);
+      int id_fun_body = get_child(node, how_many_children)->id;
+      fun_scope[arg_c] = get_child(node,1)->token ;
+      scope[arg_c][0] = get_child(node,1)->id;
+      scope[arg_c][1] = id_fun_body;
+      args[arg_c++] = get_child(node,1)->token;
+      types[type_c++] = get_child(node, how_many_children-1)->ntoken;
+      for(int i = 0; i < how_many_children-3; i++)
+      {
+         struct ast *arg = get_child(node, i+2);
+         if(arg->ntoken==NAME){
+            if(return_index(args,arg->token)!=-1){
+               printf("%s is already declared\n",arg->token);
+               return 1;
+            }
+            fun_scope[arg_c] = get_child(node,1)->token;
+            scope[arg_c][0] = arg->id;
+            scope[arg_c][1] = id_fun_body;
+            args[arg_c++] = arg->token;
+         }
+         if(arg->ntoken==INTTYPE || arg->ntoken==BOOLTYPE){
+            types[type_c++] = arg->ntoken;
+         }
+         printf("   declare its arg: %s of type %d, valid in %d -- %d\n",
+            arg->token, 
+            arg->ntoken,
+            arg->id, id_fun_body);
+      }
+   }
+   if(node->ntoken == LET){
+      printf("visit fun: %s\n", node->token);
+      struct ast *let_child = get_child(node,2);
+      int let_expr = let_child->ntoken;
+      printf("   declare its arg: %s of type %d, valid in %d -- %d\n",
+         get_child(node,1)->token,
+         let_expr,
+         get_child(node,1)->id,
+         get_child(node,3)->id);
 
-              in the body of a function if v is declared as one of the function's arguements:
-                 e.g., (define-fun f (v int) bool (= v 1)) -- allwed
-                       (define-fun g bool (= v 1)) -- not allowed (semantic error)
-           
-              in the body of a let-binder if v is declared in the let-s first argumen,
-                 e.g., (+ (let (v 1) (= v 1)) v) -- not allowed 
+      if(let_expr==ADDOP||let_expr==MINOP||let_expr==MULTOP ||
+         let_expr==LT || let_expr==GT || let_expr == EQ || 
+         let_expr == LTEQ || let_expr == GTEQ ||
+         let_expr==GETINT || let_expr==INTTYPE || let_expr==CONST)
+         types[type_c++] = INTTYPE;
+      else if(let_expr==TRUECONST || let_expr==FALSECONST ||
+         let_expr==OR || let_expr==NOT || let_expr==AND ||
+         let_expr==GETBOOL ||let_expr==BOOLTYPE)
+         types[type_c++] = BOOLTYPE;
+      else{
+         printf("Invalid Code\n");
+         return 1;
+      }
 
-           each variable name can be declared exactly once:
-              per each function,
-                 e.g., (define-fun f (v int) (v bool) ...) --  not allowed
-              a sequence of nested let-biinders,
-                 e.g., (let (x ...) (let (x ...) ...)) --  not allowed (cant redefine)
-              or both,
-                 e.g., (define-fun f (v int) ... (let (v ...) ...)) -- not allowed
-              but... (+ (let (x ...) x) (let (x ...) x)) -- allowed (stay within their scopes, not the same "x")
+      fun_scope[arg_c] = node->token;
+      scope[arg_c][0] = get_child(node,1)->id;
+      scope[arg_c][1] = get_child(node,3)->id;
+      args[arg_c++] = get_child(node,1)->token;         
+   }
+   return 0;
+}
 
-           functions can be called only if thye are previously defined (except get-int and get-bool)
-              (define-fun f (v int) bool (g))
-              (define-fun g bool (get-bool)) -- not allowed (g is used before it's declared, get-bool is allowed, should be switched)
-           
-           each function name can be used in exactly one define-fun and cannot be an input/local variable name of the defined function
-              (define-fun f (f int) bool true)
-              (define-fun f bool true)
-
-              (define-fun g (v int) bool (let (g true) g))
-       
-        (define-fun f1 (i int) bool (= i 1))
-        (define-fun f2 (i bool) (j int) int (if i j 0))
-        (print (let (x (f2 (get-bool) (get-int))) x))
-        
-        vsit fin: f1
-           visit its arg: i of type INT, valid in 2 -- 6
-          user var (of unknown type): i in location 4
-        visit fun: f2
-           declare its arg: i of type BOOL, valid in 8 -- 14 (scope)
-           declare its arg: j of type INT, valid in 9 -- 14
-          use var (of unknown type): i in location 11
-          use var (of unknown type): j in location 12
-        visit fun: main
-          declare LET var: x, scope 18 -- 18
-          use var (of unknown type) x in location 17 (bug/semantic error)
-          use var (of unknown type) x in location 18
-        */
-        
-        
-        //inclass demo function
-        //printf("visit: %s\n", node->token);
-
-        if(node->token == FUNID)
-        {
-                printf("visit fun: %s\n", node->token);
-                struct ast* parent = node->parent;
-                int how_many_children = get_children_num(parent):
-                int id_fun_body = get_child(parent, how_many_children)->id;
-                for(int i = 0; i < how_many_children - 3; i++)
-                {
-                        struct ast *arg = get_child(parent, i + 2);
-                        printf("   declare its arg: %s of type %s, valid in %d -- %d\n",
-                         arg->token, (arg->token == INTID ? "INT" : "BOOL"), arg->id, id_fun_body);
-                }
-        }
-        if(node->token == VARID)
-           printf("  use var (of unknown type): %s in location %d\n", node->token, node->id);
-        if(node->token == LETID){
-           struct ast* parent = node->parent;
-           struct ast* first_sib = get_child(parent, 2);
-           struct ast* second_sib = get_child(parent, 3);
-           printf("  declare LET var: %s, scope %d -- %d\n",
-           node->token, first_sib->id + 1, second_sib->id);
-        }
-
-
-        /*if(node->token == INTID)
-                printf("visit int var: %s\n", node->token);
-        if(node->token == BOOLID)
-                printf("visit bool var: %s\n", node->token);*/
-        return 0;
+int demo1(struct ast* node){
+   if(node->ntoken==NAME){
+      int r_index = return_index(args,node->token); 
+      printf("var: %s, fun-scope: %s, scope: %d -- %d, ID: %d\n",
+         node->token,
+         fun_scope[r_index],
+         scope[r_index][0],
+         scope[r_index][1],
+         node->id);
+      if(r_index==-1){
+         printf("%s not initialized\n",node->token);
+         return 1;
+      }
+      else if(get_child(get_root(node),1)->ntoken!=MAIN){
+         if(strcmp(fun_scope[r_index],"LET")==0){
+            if(scope[r_index][0]!=node->id && scope[r_index][1]<node->id){
+               printf("%s is not in scope\n",node->token);
+               // return 1;
+            }
+            return 0;
+         }
+         else if( strcmp(fun_scope[r_index],get_child(get_root(node),1)->token)!=0){
+            printf("%s is not in scope\n",node->token);
+            return 1;
+         }
+         
+         else{
+            return 0;
+         }
+      }
+      else if(get_child(get_root(node),1)->ntoken==MAIN && strcmp(fun_scope[r_index],"LET")==0){
+         printf("%s in not in scope\n", node->token);
+         return 1;
+      }
+   }
+   return 0;
 }
 
 int main (int argc, char **argv) {
-        int retval = yyparse();
-        //TODO: semantic analysis: symbol table + well-form, type check, etc
-        //AST -> CFG
-        //optims
-        //code generation
-        visit_ast(demo);
 
+   int retval = yyparse();
 
-        if (retval == 0) print_ast();
-        free_ast();
-        return retval;
+   if (retval == 0) {
+      int a = visit_ast(demo);
+      if(a!=0){
+         return 1;
+      }
+      a = visit_ast(demo1);
+      if(a!=0){
+         return 1;
+      }
+      print_char_array(args);
+      print_array(types);
+      print_scope(scope,0);
+      print_scope(scope,1);
+      print_ast();
+   }
+
+   free_ast();
+   return retval;
 }
